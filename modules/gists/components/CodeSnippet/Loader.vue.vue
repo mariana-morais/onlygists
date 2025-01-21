7b327091afdba3293259f7a83b2fae50efcc0fc7
Mariana <moraismariana83@gmail.com>
2025-01-21 20:06:24 -0300

    Creating dialog success modal

---
+++ modules/gists/components/CodeSnippet/Loader.vue.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col gap-2 py-8 w-full" v-if="props.loading">
+    <Skeleton width="100%" height="20rem" />
+  </div>
+
+  <slot v-else />
+</template>
+
+<script lang="ts" setup>
+   const props = withDefaults(defineProps<{
+     loading: boolean;
+  }>(), {
+    loading: false,
+  });
+</script>
+
+<style>
+
+</style>
--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,7 +1,22 @@
 <template>
     <PublicHeadline />
+    <GistCodeSnippet />
+    <DialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
 </template>
 
 <script setup lang="ts">
 import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue';
+import GistCodeSnippet from '@/modules/gists/components/CodeSnippet/CodeSnippet.vue';
+import DialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue';
+
+const route = useRoute();
+const isPaymentSuccessfully = ref<boolean>(false);
+
+onMounted(() => {
+  const { success_payment } = route.query;
+
+  if(success_payment) {
+    isPaymentSuccessfully.value = true;
+  }
+});
 </script>
